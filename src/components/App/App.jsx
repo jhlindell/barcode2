2514e5c4763d694043656c94c4e7ab4d0b08eedd
jon lindell <jhlindell@yahoo.com>
2020-05-01 13:26:21 -0600

    mobile check added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
+import { isMobile } from 'react-device-detect';
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
 import Signin from '../Login/components/SigninContainer';
@@ -19,28 +20,58 @@ const useStyles = makeStyles({
   appRoot: {
     height: '100vh',
   },
+  mobile: {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  message: {
+    display: 'flex',
+    margin: 'auto',
+  },
 });
 
 function App() {
   const classes = useStyles();
+
+  if (isMobile) {
+    return (
+      <div className={classes.mobile}>
+        <div className={classes.message}>
+          <p>
+            You cannot view this site on a mobile device. Please visit with a
+            desktop device
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={classes.appRoot}>
       <Navbar />
       <Container maxWidth="md">
         <Switch>
-          <Route path='/signin' component={Signin} />
-          <Route path='/signup' component={Signup} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
 
-          <Route exact path='/stockitems/edit/:id' component={PrivateRoute(StockItemEdit)} />
-          <Route exact path='/stockitems/create' component={PrivateRoute(StockItemCreate)} />
-          <Route exact path='/stockitems' component={StockItemList} />
-          <Route path='/stockitems/:id' component={StockItemDetail} />
+          <Route
+            exact
+            path="/stockitems/edit/:id"
+            component={PrivateRoute(StockItemEdit)}
+          />
+          <Route
+            exact
+            path="/stockitems/create"
+            component={PrivateRoute(StockItemCreate)}
+          />
+          <Route exact path="/stockitems" component={StockItemList} />
+          <Route path="/stockitems/:id" component={StockItemDetail} />
 
-          <Route exact path='/recipes' component={RecipeList} />
-          <Route path='/recipes/:id' component={RecipeDetail} />
+          <Route exact path="/recipes" component={RecipeList} />
+          <Route path="/recipes/:id" component={RecipeDetail} />
 
-          <Route exact path='/' component={Home} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </Container>
       <Messages />
